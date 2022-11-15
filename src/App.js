09f0162ef55ca96c786c6a4fa90e92feaf51e24a
Elif Balci <mrselifbalci@gmail.com>
2022-11-15 16:33:21 +0000

    title update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+const [email,setEmail]=useState('')
+
+
+const handleSubmit=(e)=>{
+  e.preventDefault()
+  console.log(email)
+}
+
   return (
     <div>
-
+       <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e)=>setEmail(e.target.value)}
+        />
+       </form>
     </div>
   );
 }
