29e6d3b371c3cbc9bd89aea37f8bb86c0d021c9b
KaliGurl <kaligurl2008@gmail.com>
2022-11-29 11:51:42 -0500

    added logos and icons to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './Header.css'
+import logo from '../../assets/House.svg';
+import unilife from '../../assets/UniLife.svg';
+import {AiOutlineHeart} from 'react-icons/ai';
+import {BiEnvelope} from 'react-icons/bi';
 
 function Header() {
 
@@ -7,16 +11,16 @@ function Header() {
   return (
     <div className='header-container'>
         <div id='logo'>
-            <image />
-            <p>UniLife</p>
+            <img src={logo} className='logo-img' alt='UniLife Logo'/>
+            <img src={unilife} alt='UniLife'/>
         </div>
         <div className='link-container'>
             <div id='favorites'>
-                <image />
+                <AiOutlineHeart className='icon'/>
                 <p>Shortlist</p>
             </div>
             <div id='contact-us'>
-                <image />
+                <BiEnvelope className='icon'/>
                 <p>Contact Us</p>
             </div>
         </div>
