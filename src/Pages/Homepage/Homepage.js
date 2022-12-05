70facb866be6009f84ff5c6f9279d06da4805403
KaliGurl <kaligurl2008@gmail.com>
2022-12-05 17:02:29 -0500

    first city api call and state check on homepage

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,8 +1,30 @@
+import axios from 'axios'
 import React from 'react'
 
 function Homepage() {
+
+  //create state for cities
+  const [cities, setCities] = React.useState([])
+
+  //call api when the component loads
+  React.useEffect(
+    ()=>{
+      //call api to get cities
+      axios.get('https://unilife-server.herokuapp.com/cities')
+      .then(res =>{
+        console.log(res.data.response);
+        setCities(res.data.response)
+      })
+      .catch(err => console.log(err))
+    },[]
+  )
+
   return (
-    <div>Homepage</div>
+    <div className='homepage-container'>
+      {
+        cities.map(item => <p>{item.name}</p>)
+      }
+    </div>
   )
 }
 
