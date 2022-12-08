06ebd8fafa1a73ca0602a3e215dddf0682c89ff1
KaliGurl <kaligurl2008@gmail.com>
2022-12-08 12:20:58 -0500

    called api in see all cities page, need to connect CityButton component and add css

---
+++ src/Components/CityButton/CityButton.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './CityButton.css'
+
+function CityButton() {
+
+
+    
+  return (
+    <div>CityButton</div>
+  )
+}
+
+export default CityButton
--- src/Pages/SeeAllCities/SeeAllCities.js
+++ src/Pages/SeeAllCities/SeeAllCities.js
@@ -1,10 +1,36 @@
 import React from 'react'
 import Banner from '../../Components/Banner/Banner'
+import CityButton from '../../Components/CityButton/CityButton'
+import axios from 'axios'
 
 function SeeAllCities() {
+
+  //create state to save city names
+   const [city, setCity] = React.useState([])
+
+
+  //call api to get city names
+  React.useEffect(
+    ()=>{
+      axios.get('https://unilife-server.herokuapp.com/cities?limit=20')
+      .then(res =>{
+        console.log(res.data.response);
+        setCity(res.data.response)
+      })
+    }, []
+  )
+
   return (
     <div className='see-all-cities-container'>
       <Banner />
+      <div className='city-buttons-container'>
+        <h3>Search by City</h3>
+        <div className='city-buttons-wrapper'>
+          {
+            city.map(item=> <CityButton city={item} />)
+          }
+        </div>
+      </div>
     </div>
   )
 }
