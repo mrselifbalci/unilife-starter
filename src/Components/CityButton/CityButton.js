e8843cf5f84bc03870e713e29e21cbb6655bde7b
KaliGurl <kaligurl2008@gmail.com>
2022-12-08 13:37:16 -0500

    finished CityButton component

--- src/Components/CityButton/CityButton.js
+++ src/Components/CityButton/CityButton.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import './CityButton.css'
 
-function CityButton() {
+function CityButton({city}) {
 
+    const buttonStyle ={
+        fontSize:"28px",
+        border:"1px black solid",
+        backgroundColor:"white",
+        borderRadius:"24px",
+        width:"302px",
+        height:"88px",
+        margin:"12px",
+    }
 
-    
   return (
-    <div>CityButton</div>
+    <div className='city-button-wrapper'>
+        <button className='button'
+        style={buttonStyle}>{city.name}</button>
+    </div>
   )
 }
 
