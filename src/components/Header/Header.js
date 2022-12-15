3bb9ba77bf212f47a01249a752263b2a1f0b8117
Ryan Carpenter <ryancarpenter@mac-mini.myfiosgateway.com>
2022-12-15 14:10:25 -0500

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header-container">
+
+        <h1>UniLife</h1>
+        <div>
+            <a href="/">Shortlist</a>
+            <a href="/">Contact Us</a>
+        </div>
+    </div>
+  )
+}
+
+export default Header
