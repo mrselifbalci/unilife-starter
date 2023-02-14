c3bc713295e01ceb3b3c29678634ecf7448b745f
Elif Balci <mrselifbalci@gmail.com>
2023-02-14 02:06:38 +0000

    removed the form from app.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,11 @@ import React,{useState} from 'react';
 import './App.css';
 
 function App() {
-const [email,setEmail]=useState('')
 
-
-const handleSubmit=(e)=>{
-  e.preventDefault()
-  console.log(email)
-}
 
   return (
     <div>
-       <form onSubmit={handleSubmit}>
-        <input
-          onChange={(e)=>setEmail(e.target.value)}
-        />
-       </form>
+
     </div>
   );
 }
